61bd3c6e919a815627e9e73564e1199c6f04f2c2
Craig Swanson <cswanson02@me.com>
2019-10-08 16:19:06 -0500

    Created view controllers and added labels to TimeCircuitsVC

---
+++ OutaTime/OutaTime/DatePickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DatePickerViewController.swift
+//  OutaTime
+//
+//  Created by Craig Swanson on 10/8/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
