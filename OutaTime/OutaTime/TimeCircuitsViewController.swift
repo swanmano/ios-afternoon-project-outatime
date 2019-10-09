5b6b8875b41e450cc44d5fc1ef00029db0f6a0ff
Craig Swanson <cswanson02@me.com>
2019-10-08 22:13:12 -0500

    simplified speed to a simple integer increment rather than a timer.

--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -25,25 +25,25 @@ class TimeCircuitsViewController: UIViewController {
         return formatter
     }()
     
-    var speedFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "ss.S"
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        return formatter
-    }()
+//    var speedFormatter: DateFormatter = {
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "ss.S"
+//        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+//        return formatter
+//    }()
     
-    var speed: TimeInterval = 0
+    var speed: Int = 0
     var lastDeparted: Date? = nil
     var timer: Timer?
-    var stopTime: Date?
-    var timeRemaining: TimeInterval {
-        if let stopTime = stopTime {
-            let timeRemaining = stopTime.timeIntervalSinceNow
-            return timeRemaining
-        } else {
-            return 0
-        }
-    }
+    var stopTime: Int = 88 // changed this to Int from Date
+//    var timeRemaining: TimeInterval {
+//        if let stopTime = stopTime {
+//            let timeRemaining = stopTime.timeIntervalSinceNow
+//            return timeRemaining
+//        } else {
+//            return 0
+//        }
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,7 +70,8 @@ class TimeCircuitsViewController: UIViewController {
     // MARK: Methods
     private func updateViews() {
         presentLabel.text = string(from: currentTime)
-        speedLabel.text = String("\(speedString(from: speed)) MPH")
+//        speedLabel.text = String("\(speedString(from: speed)) MPH")
+        speedLabel.text = String("\(speed) MPH")  // added for simple speed
         if let lastDeparted = lastDeparted {
             departedLabel.text = string(from: lastDeparted)
         } else {
@@ -81,27 +82,35 @@ class TimeCircuitsViewController: UIViewController {
     private func string(from dateEntry: Date) -> String {
         return dateFormatter.string(from: dateEntry)
     }
-    private func speedString(from speedEntry: TimeInterval) -> String {
-        let date = Date(timeIntervalSinceReferenceDate: speed)
-        return speedFormatter.string(from: date)
-    }
+//    private func speedString(from speedEntry: TimeInterval) -> String {
+//        let date = Date(timeIntervalSinceReferenceDate: speed)
+//        return speedFormatter.string(from: date)
+//    }
     
     private func startTimer() {
         timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(timer:))
-        stopTime = Date(timeIntervalSinceNow: 8)
+//        stopTime = Date(timeIntervalSinceNow: 8)
     }
     
     private func updateSpeed(timer: Timer) {
-        if let stopTime = stopTime {
-            let currentTime = Date()
-            if currentTime <= stopTime {
-                speed = timeRemaining
-                updateViews()
-            } else {
-                resetTimer()
-            }
+        if speed < stopTime {
+            speed += 2
+            updateViews()
+        } else {
+            resetTimer()
         }
     }
+//    private func updateSpeed(timer: Timer) {
+//        if let stopTime = stopTime {
+//            let currentTime = Date()
+//            if currentTime <= stopTime {
+//                speed = timeRemaining
+//                updateViews()
+//            } else {
+//                resetTimer()
+//            }
+//        }
+//    }
     
     func resetTimer() {
         timer?.invalidate()
